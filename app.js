5cceb2f92b9cbe08ba4896724327196b309581a4
Masad Rayan <masadrayan2002@gmail.com>
2026-02-13 11:05:06 +0600

    feat: enhance sign-up endpoint validation using zod schema

--- app.js
+++ app.js
@@ -1,28 +1,37 @@
-const express = require('express')
-const cors = require('cors')
-const app = express()
-const port = 3000
+const express = require("express");
+const cors = require("cors");
+const { z } = require("zod");
+const app = express();
+const port = 3000;
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
+app.post("/auth/sign-up", (req, res) => {
+  const userCreateSchema = z.object({
+    firstName: z.string().min(3),
+    lastName: z.string().min(3),
+    email: z.email(),
+    password: z.string().min(8),
+  });
 
-app.post("/auth/sign-up",  (req, res) => {
-  if (req.body.firstName.length <= 2) {
+  const { success, data, error } = userCreateSchema.safeParse(req.body);
+
+  if (!success) {
     return res.status(400).json({
-      error: "First name must be at least 3 characters long"
+      error: "Invalid input data",
     })
   }
-  res.json({
-    user: req.body
-  })
-})
 
+  res.json({
+    user: req.body,
+  });
+});
 
 app.get("/", async (req, res) => {
   res.send("Hello World!");
-})
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});

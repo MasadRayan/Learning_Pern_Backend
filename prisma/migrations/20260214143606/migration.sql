6f28c865744f67ae9de3a076c90ca3c05ba7d554
Masad Rayan <masadrayan2002@gmail.com>
2026-02-14 20:54:16 +0600

    feat: add migration for Product, User, ProductImage, and ProductVariant tables; update primary keys and foreign key constraints

---
+++ prisma/migrations/20260214143606/migration.sql
@@ -0,0 +1,55 @@
+/*
+  Warnings:
+
+  - The primary key for the `Product` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - The primary key for the `User` table will be changed. If it partially fails, the table could be left without primary key constraint.
+  - Changed the type of `id` on the `Product` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+  - Changed the type of `id` on the `User` table. No cast exists, the column would be dropped and recreated, which cannot be done if there is data, since the column is required.
+
+*/
+-- AlterTable
+ALTER TABLE "Product" DROP CONSTRAINT "Product_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" UUID NOT NULL,
+ADD CONSTRAINT "Product_pkey" PRIMARY KEY ("id");
+
+-- AlterTable
+ALTER TABLE "User" DROP CONSTRAINT "User_pkey",
+DROP COLUMN "id",
+ADD COLUMN     "id" UUID NOT NULL,
+ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+
+-- CreateTable
+CREATE TABLE "ProductImage" (
+    "id" UUID NOT NULL,
+    "productId" UUID NOT NULL,
+    "imageUrl" VARCHAR(255) NOT NULL,
+    "altText" VARCHAR(255),
+    "displayOrder" INTEGER NOT NULL DEFAULT 0,
+    "isPrimary" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ProductImage_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "ProductVariant" (
+    "id" UUID NOT NULL,
+    "productId" UUID NOT NULL,
+    "variantName" VARCHAR(255) NOT NULL,
+    "variantValue" VARCHAR(255) NOT NULL,
+    "priceAdjustment" DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+    "stockQuantity" INTEGER NOT NULL DEFAULT 0,
+    "imageUrl" VARCHAR(255),
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "ProductVariant_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "ProductImage" ADD CONSTRAINT "ProductImage_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "ProductVariant" ADD CONSTRAINT "ProductVariant_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;

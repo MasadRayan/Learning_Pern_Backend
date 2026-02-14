97b218685b2090698335ad696b7ee846aded1669
Masad Rayan <masadrayan2002@gmail.com>
2026-02-14 23:20:32 +0600

    feat: implement CRUD operations for images; add create, update, and delete functions in image controller and update routes

--- src/controller/product/imageController.js
+++ src/controller/product/imageController.js
@@ -59,3 +59,157 @@ export const getImageById = async (req, res) => {
         data: { image },
     });
 }
+
+export const createImage = async (req, res) => {
+
+    const imageSchema = z.object({
+        productId: z.uuid(),
+        imageUrl: z.url(),
+        altText: z.string().optional(),
+        displayOrder: z.number().int().nonnegative().optional(),
+        isPrimary: z.boolean().default(false).optional(),
+    })
+
+    const { success, data, error } = imageSchema.safeParse(req.body);
+
+    if (!success) {
+        return res.status(400).json({
+            status: "error",
+            message: "Invalid image data",
+            errors: error.errors,
+        });
+    }
+
+    const isProductExists =await prisma.product.findUnique({
+        where: {
+            id: data.productId,
+        },
+    });
+
+    if (!isProductExists) {
+        return res.status(404).json({
+            status: "error",
+            message: "Product not found",
+        });
+    }
+
+    const newImage = await prisma.productImage.create({
+        data: {
+            productId: data.productId,
+            imageUrl: data.imageUrl,
+            altText: data.altText,
+            displayOrder: data.displayOrder,
+            isPrimary: data.isPrimary,
+        },
+    });
+
+    return res.json({
+        status: "success",
+        message: "Image created successfully",
+        data: { image: newImage },
+    });
+    
+
+}
+
+export const deleteImage = async (req, res) => {
+    const imageId = req.params.id;
+
+    const imageSchema = z.object({
+        id: z.uuid(),
+    });
+
+    const { success, data, error } = imageSchema.safeParse({ id: imageId });
+
+    if (!success) {
+        return res.status(400).json({
+            status: "error",
+            message: "Invalid image ID",
+        });
+    }
+
+    const isImageExists = await prisma.productImage.findUnique({
+        where: {
+            id: data.id,
+        },
+    });
+
+    if (!isImageExists) {
+        return res.status(404).json({
+            status: "error",
+            message: "Image not found",
+        });
+    }
+
+    const deletedImage = await prisma.productImage.delete({
+        where: {
+            id: data.id,
+        },
+    });
+
+    if (!deletedImage) {
+        return res.status(404).json({
+            status: "error",
+            message: "Image not found",
+        });
+    }
+
+    return res.json({
+        status: "success",
+        message: "Image deleted successfully",
+        data: { image: deletedImage },
+    });
+}
+
+export const updateImage = async (req, res) => {
+    const imageId = req.params.id;
+
+    const imageSchema = z.object({
+        id: z.uuid(),
+        imageUrl: z.url().optional(),
+        altText: z.string().optional(),
+        displayOrder: z.number().int().nonnegative().optional(),
+        isPrimary: z.boolean().optional(),
+    });
+
+    const { success, data, error } = imageSchema.safeParse({ id: imageId, ...req.body });
+
+    if (!success) {
+        return res.status(400).json({
+            status: "error",
+            message: "Invalid image data",
+            errors: error.errors,
+        });
+    }
+
+    const isImageExists = await prisma.productImage.findUnique({
+        where: {
+            id: data.id,
+        },
+    });
+
+    if (!isImageExists) {
+        return res.status(404).json({
+            status: "error",
+            message: "Image not found",
+        });
+    }
+
+    const updatedImage = await prisma.productImage.update({
+        where: {
+            id: data.id,
+        },
+        data: {
+            imageUrl: data.imageUrl,
+            altText: data.altText,
+            displayOrder: data.displayOrder,
+            isPrimary: data.isPrimary,
+        },
+    });
+
+    return res.json({
+        status: "success",
+        message: "Image updated successfully",
+        data: { image: updatedImage },
+    });
+}
--- src/routes/Product/imageRoute.js
+++ src/routes/Product/imageRoute.js
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { getAllImages } from "../../controller/product/index.js";
+import { getAllImages, getImageById, createImage, deleteImage, updateImage } from "../../controller/product/index.js";
 const router = Router();
 
 router.get("/", getAllImages);
-
+router.get("/:id", getImageById);
+router.post("/", createImage);
+router.patch("/:id", updateImage);
+router.delete("/:id", deleteImage);
 
 export default router;

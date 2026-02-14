2bfc5f5641c56519293a3dcad65beb178424d7c6
Masad Rayan <masadrayan2002@gmail.com>
2026-02-14 22:49:43 +0600

    feat: add getImageById function to image controller; implement image ID validation and fetching logic

--- src/controller/product/imageController.js
+++ src/controller/product/imageController.js
@@ -23,3 +23,39 @@ export const getAllImages = async (req, res) => {
     });
 
 };
+
+export const getImageById = async (req, res) => {
+    const imageId = req.params.id;
+
+    const imageSchema = z.object({
+        id: z.uuid(),
+    });
+
+    const {success, data, error} = imageSchema.safeParse({ id: imageId });
+
+    if (!success) {
+        return res.status(400).json({
+            status: "error",
+            message: "Invalid image ID",
+        });
+    }
+
+    const image = await prisma.productImage.findUnique({
+        where: {
+            id: data.id,
+        },
+    });
+
+    if (!image) {
+        return res.status(404).json({
+            status: "error",
+            message: "Image not found",
+        });
+    }
+
+    return res.json({
+        status: "success",
+        message: "Image fetched successfully",
+        data: { image },
+    });
+}

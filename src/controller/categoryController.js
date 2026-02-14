5b5b09fe1d386551ae7bdbc990421becb1848251
Masad Rayan <masadrayan2002@gmail.com>
2026-02-14 11:30:28 +0600

    feat: add category and product controllers with basic CRUD operations

---
+++ src/controller/categoryController.js
@@ -0,0 +1,19 @@
+export const getAllCategories = async (req, res) => {
+    return "ALL Category"
+}
+
+export const getACategory = async (req, res) => {
+    return "A Category"
+}
+
+export const createCategory = async (req, res) => {
+    return "Create Category"
+}
+
+export const updateCategory = async (req, res) => {
+    return "Update Category"
+}
+
+export const deleteCategory = async (req, res) => {
+    return "Delete Category"
+}
---
+++ src/controller/productController.js
@@ -0,0 +1,19 @@
+export const getAllProducts = async (req, res) => {
+    return "ALL Products"
+}
+
+export const getAProduct = async (req, res) => {
+    return "A Product"
+}
+
+export const createProduct = async (req, res) => {
+    return "Create Product"
+}
+
+export const updateProduct = async (req, res) => {
+    return "Update Product"
+}
+
+export const deleteProduct = async (req, res) => {
+    return "Delete Product"
+}

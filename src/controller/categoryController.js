4f9f510824a038d0f0717a9c5820d38737e867e4
Masad Rayan <masadrayan2002@gmail.com>
2026-02-14 13:07:58 +0600

    feat: make slug and imageUrl fields optional in Category and Product models; update controllers for improved response handling

--- src/controller/categoryController.js
+++ src/controller/categoryController.js
@@ -1,11 +1,20 @@
 import { prisma } from "../database/prisma.js";
 import { z } from "zod";
-import bcrypt from "bcrypt";
-import jwt from "jsonwebtoken";
+
 
 
 export const getAllCategories = async (req, res) => {
-    return res.send("ALL Category")
+    const allCategories = await prisma.category.findMany({
+        orderBy: {
+            createdAt: "desc",
+        },
+    });
+
+    return res.json({
+        status: "success",
+        message: "Categories fetched successfully",
+        data: { categories: allCategories },
+    });
 }
 
 export const getACategory = async (req, res) => {
@@ -13,7 +22,56 @@ export const getACategory = async (req, res) => {
 }
 
 export const createCategory = async (req, res) => {
-    return res.send("Create Category")
+    const categorySchema = z.object({
+        name: z.string().min(1),
+        description: z.string().optional(),
+        slug: z.string().min(1).optional(),
+        parentId: z.uuid().optional(),
+        imageUrl: z.url().optional(),
+    });
+
+    const { success, data, error } = categorySchema.safeParse(req.body);
+
+    if (!success) {
+        return res.status(400).json({
+            status: "error",
+            message: "Invalid request data",
+        });
+    }
+
+    // check if parent category exists
+    // if (data.parentId) {
+    //     const parentCategoryExists = await prisma.category.findUnique({
+    //         where: {
+    //             id: data.parentId,
+    //         },
+    //     });
+
+    //     if (!parentCategoryExists) {
+    //         return res.status(400).json({
+    //             status: "error",
+    //             message: "Parent category does not exist",
+    //         });
+    //     }
+    // }
+
+    const categoryPayload = {
+        name: data.name,
+        slug: data.slug,
+        parentId: data.parentId,
+        description: data.description,
+        imageUrl: data.imageUrl,
+    };
+
+    const newCategory = await prisma.category.create({
+        data: categoryPayload,
+    });
+
+    return res.json({
+        status: "success",
+        message: "Category created successfully",
+        data: { category: newCategory },
+    });
 }
 
 export const updateCategory = async (req, res) => {
--- src/controller/productController.js
+++ src/controller/productController.js
@@ -3,35 +3,86 @@ import { z } from "zod";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 
-
 export const getAllProducts = async (req, res) => {
+  const allProducts = await prisma.product.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
 
-     const allProducts = await prisma.product.findMany({
-        orderBy: {
-            createdAt: "desc",
-        }
-     })
-
-    return res.json({
-        status: "success",
-        message: "Products fetched successfully",
-        data: { products: allProducts },
-    })
-}
-
+  return res.json({
+    status: "success",
+    message: "Products fetched successfully",
+    data: { products: allProducts },
+  });
+};
 
 export const getAProduct = async (req, res) => {
-    return res.send("A Product")
-}
+  return res.send("A Product");
+};
 
 export const createProduct = async (req, res) => {
-    return res.send("Create Product")
-}
+  const productSchema = z.object({
+    name: z.string().min(1),
+    slug: z.string().min(1).optional(),
+    description: z.string().optional(),
+    categoryId: z.uuid(),
+    basePrice: z.number().min(1),
+    originalPrice: z.number().min(1).optional(),
+    stockQuantity: z.number().min(1),
+    
+  });
+
+  const { success, data, error } = productSchema.safeParse(req.body);
+
+  if (!success) {
+    return res.status(400).json({
+      status: "error",
+      message: "Invalid request data",
+    });
+  }
+
+  // check if category exists
+  const categoryExists = await prisma.category.findUnique({
+    where: {
+      id: data.categoryId,
+    },
+  });
+
+  if (!categoryExists) {
+    return res.status(400).json({
+      status: "error",
+      message: "Category does not exist",
+    });
+  }
+
+    const productPayload = {
+    name: data.name,
+    slug: data.slug,
+    description: data.description,
+    categoryId: data.categoryId,
+    basePrice: data.basePrice,
+    originalPrice: data.originalPrice,
+    stockQuantity: data.stockQuantity,
+  };
+
+  const createProduct = await prisma.product.create({
+    data: productPayload,
+  })
+
+  res.json({
+    status: "success",
+    message: "Product created successfully",
+    data: { product: createProduct },
+  })
+
+
+};
 
 export const updateProduct = async (req, res) => {
-    return res.send("Update Product")
-}
+  return res.send("Update Product");
+};
 
 export const deleteProduct = async (req, res) => {
-    return res.send("Delete Product")
-}
+  return res.send("Delete Product");
+};
